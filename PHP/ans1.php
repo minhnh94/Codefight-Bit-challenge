4305f771cf7addb4e44e380ae9fddfb8473b7eeb
minhnh_bkhn <minhnh.da@gmail.com>
2017-10-22 00:48:47 +0900

    solve answer 1

--- PHP/ans1.php
+++ PHP/ans1.php
@@ -1,5 +1,12 @@
-//in this problem, can only edit the return statement.
-
+<?php
 function killKthBit($n, $k) {
-  return ... ;
+	$nInBase2 = base_convert($n, 10, 2);
+	// echo $nInBase2 . "<br>";
+	$kInBase2 = base_convert(2 ** ($k-1), 10, 2);
+	// echo $kInBase2 . "<br>";
+	
+	return $nInBase2 ^ $kInBase2;
 }
+
+echo killKthBit(37,3);
+// echo 0b100101^0b100;
